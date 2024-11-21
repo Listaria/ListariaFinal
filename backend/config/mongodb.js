2780a5d3d319244a94799a9717343298a57edf7a
sagarchaurasia176 <101509099+sagarchaurasia176@users.noreply.github.com>
2024-11-22 01:27:08 +0530

    backend mongo db chnage

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,13 +1,19 @@
 import mongoose from "mongoose";
+import "dotenv/config";
 
+// coonect db
 const connectDB = async () => {
+  try {
+    mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Db worked fine");
 
-    mongoose.connection.on('connected',() => {
-        console.log("DB Connected");
-    })
-
-    await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`)
-
-}
+  } catch (er) {
+    console.error("Error connecting to MongoDB:", err.message);
+    process.exit(1); // Exit process with failure
+  }
+};
 
 export default connectDB;
