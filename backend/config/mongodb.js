600464e8c902ae87efc0ae42c419f63fad993557
manish-lal12 <manishlal2274@gmail.com>
2024-11-22 23:25:16 +0530

    Initial commit

---
+++ backend/config/mongodb.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import "dotenv/config";
+
+// coonect db
+const connectDB = async () => {
+  try {
+    mongoose.connect("mongodb+srv://sagarchaurasialink:t9cJDM5CS4UOqgNZ@cluster0.btqwc.mongodb.net", {
+  
+    });
+    console.log("Db worked fine");
+
+  } catch (er) {
+    console.error("Error connecting to MongoDB:", err.message);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+export default connectDB;
---
+++ backend/server.js
@@ -0,0 +1,48 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+import userRouter from "./routes/userRoute.js";
+import productRouter from "./routes/productRoute.js";
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
+// import favicon from "serve-favicon";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// App Config
+const app = express();
+const port = process.env.PORT || 4000;
+connectDB();
+connectCloudinary();
+
+// Middlewares
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*", // Replace with your frontend domain
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+    credentials: true, // Include cookies in the requests if needed
+  })
+);
+// app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+
+// API Endpoints
+app.use("/api/user", userRouter);
+app.use("/api/product", productRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
+
+app.get("/", (req, res) => {
+  res.send("Listaria backend working");
+});
+
+app.listen(port, () => {
+  console.log("server is listening on port 4000...");
+});
+// Export app for serverless environments
+export default app;
